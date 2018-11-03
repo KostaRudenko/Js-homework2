73e7e424b7a95445f86134e8ca48331ce4a03c66
KostaRudenko <goglemoogle@gmail.com>
2018-11-03 18:19:29 +0200

    added '', ``, ""

--- mainJs.js
+++ mainJs.js
@@ -1,14 +1,12 @@
-let str = "function(){}[]";
+let str = "function(''){\"\"}[``]";
 
 validSequence = (string) => {
     let stack = [];
 
     for (let i = 0; i < string.length; i++) {
-        //console.log(string[i])
-        if (string[i] === '[' || string[i] === '(' || string[i] === '{') { // если на входе скобка открыв.,
+        if (string[i] === '[' || string[i] === '(' || string[i] === '{' || string[i] === '\'' && stack.indexOf('\'') === -1 || string[i] === '"' && stack.indexOf('"') === -1 || string[i] === '`' && stack.indexOf('`') === -1) { // если на входе скобка открыв.,
             stack.push(string[i]); //то добавляем в стэк
-            //console.log(stack)
-        } else if (string[i] === ']' || string[i] === ')' || string[i] === '}') { //если на входе закрыв. скобка
+        } else if (string[i] === ']' || string[i] === ')' || string[i] === '}' || string[i] === '\'' || string[i] === '"' || string[i] === '`') { //если на входе закрыв. скобка
             if (stack.length > 0) { // и стэк не пустой
                 stack.pop(); //удаляем скобку
             } else return false  //если закрывающаяся на входе и стэк пустой
